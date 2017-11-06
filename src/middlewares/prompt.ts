97ca068fa2ce1a9e335f9308defe3668ec119152
Fabio Spampinato <spampinabio@gmail.com>
2017-11-06 17:30:59 +0100

    Fixed support for boolean types

--- src/middlewares/prompt.ts
+++ src/middlewares/prompt.ts
@@ -12,7 +12,11 @@ async function prompt ( files, metalsmith, next ) {
   const metadata = metalsmith.metadata (),
         variablesOrder = metadata.schema.variablesOrder || [],
         variablesNames = variablesOrder.concat ( _.sortBy ( _.difference ( Object.keys ( metadata.schema.variables ), variablesOrder ), [x => x.toLowerCase ()] ) ),
-        variablesValues = await series ( variablesNames.map ( name => () => ask.input ( `${name}:`, _.get ( metadata.schema.variables[name], 'default' ) ) ) ),
+        variablesValues = await series ( variablesNames.map ( name => () => {
+          const schemaType = _.get ( metadata, `schema.variables.${name}.type` ),
+                type = _.isString ( schemaType ) && ask.hasOwnProperty ( schemaType ) ? schemaType : 'input';
+          return ask[type] ( `${name}:`, _.get ( metadata.schema.variables[name], 'default' ) )
+        })),
         variables = _.zipObject ( variablesNames, variablesValues );
 
   metadata.renderVariables = variables;
